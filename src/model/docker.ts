64323f03f817a447679fe64d0e94162fc6685c4c
John Soros <john.soros@onibi.gg>
2025-07-03 19:51:10 -0500

    cache to workspace folder

--- src/model/docker.ts
+++ src/model/docker.ts
@@ -105,7 +105,7 @@ class Docker {
             ${ImageEnvironmentFactory.getEnvVarString(parameters)} \
             --env GITHUB_WORKSPACE=c:${dockerWorkspacePath} \
             ${gitPrivateToken ? `--env GIT_PRIVATE_TOKEN="${gitPrivateToken}"` : ''} \
-            --env BEE_CACHE_DIRECTORY=c:/BlankProject/bee_cache \
+            --env BEE_CACHE_DIRECTORY=c:${dockerWorkspacePath}/bee_cache \
             --volume "${workspace}":"c:${dockerWorkspacePath}" \
             --volume "c:/regkeys":"c:/regkeys" \
             --volume "C:/Program Files/Microsoft Visual Studio":"C:/Program Files/Microsoft Visual Studio" \
